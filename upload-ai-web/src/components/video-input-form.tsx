f044bbb81da4601da6c3dcbe7927894eb4429437
Henrique Cole Fernandes <henriquecolefernandes@gmail.com>
2023-09-21 14:35:47 -0300

    feat: AI generating by the prompts

--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -17,7 +17,11 @@ const statusMessages = {
   success: "Video uploaded successfully!",
 };
 
-export function VideoInputForm() {
+interface VideoInputFormProps {
+  onVideoUploaded: (id: string) => void;
+}
+
+export function VideoInputForm(props: VideoInputFormProps) {
   const [videoFile, setVideoFile] = useState<File | null>(null);
   const [status, setStatus] = useState<Status>("waiting");
 
@@ -91,6 +95,8 @@ export function VideoInputForm() {
     });
 
     setStatus("success");
+
+    props.onVideoUploaded(videoId);
   }
 
   function handleFileSelect(event: React.ChangeEvent<HTMLInputElement>) {
